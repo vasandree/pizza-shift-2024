dc9e85b819136ae8aaadd5fe94f3e178a5433443
Vasandr15 <vasandrey007@gmail.com>
2024-07-06 01:54:38 +0700

    removed FC from icons

--- pizza delivery/src/Icons/CartIcon.tsx
+++ pizza delivery/src/Icons/CartIcon.tsx
@@ -1,6 +1,4 @@
-import {FC} from "react";
-
-export const CartIcon: FC = () => (
+export const CartIcon = () => (
 
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
--- pizza delivery/src/Icons/LogoIcon.tsx
+++ pizza delivery/src/Icons/LogoIcon.tsx
@@ -1,6 +1,4 @@
-import {FC} from "react";
-
-export const LogoIcon: FC = () =>
+export const LogoIcon = () =>
     (
         <svg width="102" height="40" viewBox="0 0 102 40" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
